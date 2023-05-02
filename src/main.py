4261b839b52d7c950a5f1d7fc6c0c332648144b7
Rishav Mitra <rishavmitra10@gmail.com>
2023-05-02 16:52:23 +0530

    fix: Path parameters and add releases workflow (#7)
    * fix for newer versions
    
    * move api code to separate directory
    
    * feat: add releases workflow
    
    * chore(release): v0.1.0
    
    * feat: remove notify.py and covid.ico
    
    * ci: switch branch to main
    
    ---------
    
    Co-authored-by: Conventional Changelog Action <conventional.changelog.action@github.com>

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI, Path
 
-from scraper import scrape
+from .scraper import scrape
 
 app = FastAPI()
 
 
 @app.get("/{country_name}")
-def home(country_name: str = Path(None, description="The name of the country you want to search")):
+def home(country_name: str | None = None):
+	"""
+	country_name: The name of the country you want to search
+	"""
 	data = scrape(country_name)
 
 	if data is not None:
